6e783aa97d598014a0e402030276fd84ec0887bc
May Miller-Ricci <maymillerricci@gmail.com>
2016-08-10 00:34:37 -0700

    start pagination adding li page number links

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+var studentList = $(".student-list");
+var studentCount = $(".student-list li").length;
+var perPageCount = 10;
+
+function getPageCount(total, perPage) {
+  return Math.ceil(total / perPage);
+}
+
+var pageCount = getPageCount(studentCount, perPageCount);
+
+function paginate() {
+  studentList.after("<div class='pagination'></div>");
+  for (var i = 1; i <= pageCount; i++) {
+  	$(".pagination").append("<li><a href='#'>" + i + "</a></li>");
+  }
+}
+
+if (pageCount > 1) {
+  paginate();
+}
 
