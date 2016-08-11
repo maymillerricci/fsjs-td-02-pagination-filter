0f8e58f1ea16abd87f4be644f182ea8e3f54a5e2
May Miller-Ricci <maymillerricci@gmail.com>
2016-08-10 18:49:37 -0700

    implement pagination - show just the 10 students on that page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var studentList = $(".student-list");
-var studentCount = $(".student-list li").length;
+var studentListItems = $(".student-list li");
+var studentCount = studentListItems.length;
 var perPageCount = 10;
 
 function getPageCount(total, perPage) {
@@ -11,11 +12,26 @@ var pageCount = getPageCount(studentCount, perPageCount);
 function paginate() {
   studentList.after("<div class='pagination'></div>");
   for (var i = 1; i <= pageCount; i++) {
-  	$(".pagination").append("<li><a href='#'>" + i + "</a></li>");
+    $(".pagination").append("<li><a href='#'>" + i + "</a></li>");
   }
 }
 
 if (pageCount > 1) {
   paginate();
+  goToPage(1)
 }
 
+$(".pagination a").on("click", function(e) {
+  e.preventDefault();
+  var pageNumber = $(this).text();
+  goToPage(pageNumber);
+});
+
+function goToPage(pageNumber) {
+  var startIndex = perPageCount * (pageNumber - 1)
+  var endIndex = perPageCount * pageNumber
+  var studentsToShow = studentListItems.slice(startIndex, endIndex)
+  studentListItems.hide();
+  studentsToShow.show();
+}
+  
